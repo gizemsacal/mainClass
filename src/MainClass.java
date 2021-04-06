7bcaf0aaab9ad7732003e7b960a2ee207718f8c3
gizemsacal <46420726+gizemsacal@users.noreply.github.com>
2021-04-06 16:36:30 +0300

    Sum int example

---
+++ src/MainClass.java
@@ -0,0 +1,11 @@
+public class MainClass {
+    public static void main(String[] args) {
+        int sayi1 =7;
+        int sayi2 =5;
+        int sayi3 =12;
+        int sum;
+       sum =sayi1+sayi2+sayi3;
+        System.out.println(sayi1 + " + " + sayi2 + " + " +sayi3 + " = " + sum);
+    }
+
+}
